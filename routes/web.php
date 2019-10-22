5e26c7be7af4518f30a8a93a57dd13c3a50b3e46
TANAKA RYOKO <ryochi0825@yahoo.co.jp>
2019-10-22 06:42:03 +0000

    PHP/Laravel 11 課題

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,16 @@ Route::get('/', function () {
 
 
 Route::group(["prefix" => "admin"],function(){
-    Route::get("news/create","Admin\NewsController@add");
+    Route::get("news/create","Admin\NewsController@add")->middleware('auth');
 });
 
 Route::group(["prefix" => "admin"],function(){
-    Route::get('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/create', 'Admin\ProfileController@add');
 });
 
 Route::group(["prefix" => "admin"],function(){
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
