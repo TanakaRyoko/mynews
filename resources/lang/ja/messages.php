152080729a44e0acab8e5f9a04ec9e55724c96f1
TANAKA RYOKO <ryochi0825@yahoo.co.jp>
2019-10-23 15:08:20 +0000

    php laravel 11課題再提出

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return[
+    'Login' => 'ログイン',
+    'E-mail Address' => 'メールアドレス',
+    'Password' => 'パスワード',
+    'Remember Me' => 'ログイン情報を記憶する',
+    ];
--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,11 @@ Route::group(["prefix" => "admin"],function(){
 });
 
 Route::group(["prefix" => "admin"],function(){
-    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add')-> middleware('auth');
 });
 
 Route::group(["prefix" => "admin"],function(){
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')-> middleware('auth');
 });
 Auth::routes();
 
