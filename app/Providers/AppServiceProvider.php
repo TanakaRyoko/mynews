6f38b82256d956cdb84338bd7ba72d9a7da6ae38
TANAKA RYOKO <ryochi0825@yahoo.co.jp>
2019-11-14 21:56:48 +0000

    heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+     
     public function boot()
-    {
-        //
+{
+    if (\App::environment('production')) {
+        \URL::forceScheme('https');
     }
 }
+
